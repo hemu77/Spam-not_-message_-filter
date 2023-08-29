e11396b08a151a12192123ea79cc9d9053a3688c
Kilaru sai hemanth <102619125+hemu77@users.noreply.github.com>
2023-08-29 20:44:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pickle
 import string
+import nltk
+
 nltk.download('punkt')
 nltk.download('stopwords')
 from nltk.corpus import stopwords
-import nltk
-
 
 
 from nltk.stem.porter import PorterStemmer
